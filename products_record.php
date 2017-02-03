f38ce7cd5be59b0d9c8f2904aac3cf4cabe7dfae
avinashneo <callavinash1@gmail.com>
2017-02-03 10:31:56 +0530

    Update products_record.php

--- products_record.php
+++ products_record.php
@@ -9,23 +9,45 @@ class Record {
 		$this->record = array();
 		
 	}
-        
+        /**
+	* add the product into the system.
+	* @param string $productName the product's name to enter into system.		
+	*
+	*/
         public function addRecord($productName) {
 		
             if (!$this->isInRecord($productName))
                 $this->record[$productName] = new Products($productName);
 
 	}
-	
+	/**
+	* check the product into the system.
+	* @param string $productName the product's name to enter into system.
+	*
+	* @return boolean True if the system is found the product		
+	* @return boolean False if the system is not found the product
+	*/
 	public function isInRecord($productName) {
 		return array_key_exists($productName, $this->record);
 	}
-        
+        /**
+	* get the product into the system.
+	* @param string $productName the product's name to enter into system.
+	*
+	*
+	*/
         public function get($productName) {
 		if ($this->isInRecord($productName))
                         return $this->record[$productName]; 
 	}
-        
+        /**
+	* Adds the product into the system.
+	* @param string $productName the product's name to enter into system.
+	*
+	* @return boolean True if the system was able to add the product into the system, 
+	* @return boolean False if the system wasn't able to add the product.			
+	*
+	*/
         public function scan($productName) {
             
 		if ($this->getProductFromRecord($productName)) {
